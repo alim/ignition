198e26213c041ffa1eb916160889a1628578f372
Andy Lim <alim@nspartners.com>
2013-08-28 17:47:52 -0400

    Issue #11 - Updated validations for User model to include last_name and phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,11 +52,11 @@ class User
   field :first_name, type: String, default: ''
   validates_presence_of :first_name
   
-  field :last_name, type: String
-#  validates_presence_of :last_name
+  field :last_name, type: String, default: ''
+  validates_presence_of :last_name
   
-  field :phone, type: String
-#  validates_presence_of :phone
+  field :phone, type: String, default: ''
+  validates_presence_of :phone
   
   field :role, type: Integer, default: User::CUSTOMER
   
