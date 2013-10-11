974719f7b310cc6dd3aae0747bbb1608b9b75af9
Andy Lim <alim@nspartners.com>
2013-10-11 12:42:53 -0400

    Issue #24 - Backout asset precompile options.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,11 @@ Ignition::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assests.precompile += %w( 
-    fontawesome-webfont.eot, 
-    fontawesome-webfont.woff,
-    fontawesome-webfont.ttf
-  )
+#  config.assests.precompile += %w( 
+#    fontawesome-webfont.eot, 
+#    fontawesome-webfont.woff,
+#    fontawesome-webfont.ttf
+#  )
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
