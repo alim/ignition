c5871c40489df4e7f218261fb0ab7e2eb8d8b424
Andy Lim <alim@nspartners.com>
2013-10-14 11:07:52 -0400

    Issue #24 - Adding precompile options for font awesome

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,11 @@ Ignition::Application.configure do
 #    fontawesome-webfont.woff,
 #    fontawesome-webfont.ttf
 #  )
-  
+  config.assets.precompile += ["fontawesome-webfont.ttf",
+                         "fontawesome-webfont.eot",
+                         "fontawesome-webfont.svg",
+                         "fontawesome-webfont.woff"]
+                         
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
