46e3c5df49819545fa18291ed3345d1e377a14b7
Andy Lim <alim@nspartners.com>
2014-05-14 10:47:13 -0400

    Issue #76 - Updated application_helper_spec to match new set active helper.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,22 +4,22 @@ describe ApplicationHelper do
   describe "#active method tests" do
     it "should return active string, if the path matches" do
       helper.request.path = admin_path
-      helper.active(admin_path).should == "active"
+      helper.active(admin_path).should == "class=active"
     end
 
     it "if group path and settings selected, should return active" do
       helper.request.path = groups_path
-      helper.active('/settings').should == "active"
+      helper.active('/settings').should == "class=active"
     end
 
     it "if project path and settings selected, should return active" do
       helper.request.path = projects_path
-      helper.active('/settings').should == "active"
+      helper.active('/settings').should == "class=active"
     end
 
     it "if edit user path and settings selected, should return active" do
       helper.request.path = edit_user_registration_path
-      helper.active('/settings').should == "active"
+      helper.active('/settings').should == "class=active"
     end
 
     it "should return empty string, if the path does not match" do
